1195db3ac51d3b2139da230ed5ed9b6825920c83
shunyue1320 <1277952981@qq.com>
2020-12-01 23:02:05 +0800

    修复bin入口

--- bin/index.js
+++ bin/index.js
@@ -3,6 +3,7 @@ const path = require('path')
 const { promisify } = require('util')
 const open = require("open")
 const clear = require('clear')
+const chalk = require('chalk')
 const figlet = require('figlet')
 const program = require('commander')
 const chalkAnimation = require('chalk-animation')
@@ -14,26 +15,28 @@ program.version(require('../package.json').version)
 program
   .command('dev')
   .description('本地运行开发环境项目')
-  .usage('[sourceDir] root-path')
+  .usage('[options] root-path')
   .option('-t, --theme [theme]', 'Markdown theme is default or techo')
   .option('-p, --port [port]', '开启服务器端口')
-  .action(async (sourceDir) => {
+  .action(async (options) => {
     clear()
     console.log('')
     
     const uabpress = await figletAsync('uabpress')
     chalkAnimation.pulse(uabpress).start()
 
-    const port = sourceDir.port || 3000
+    const port = options.port || 3000
     wrapCommand({
       port,
       theme: options.theme || 'default',
-      sourceDir: path.resolve(sourceDir.args.length > 0 ? sourceDir.args[0] : '.')
+      sourceDir: path.resolve(options.args.length > 0 ? options.args[0] : '.')
     })
 
-    await open(`http://localhost: ${port}`)
+    await open(`http://localhost:${port}`)
     console.log(' Dev server running at:')
     console.log('')
     console.log(` > Local: ${chalk.cyan(`http://localhost: ${port}`)}`)
     console.log('')
   })
+
+program.parse(process.argv)
