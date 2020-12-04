4a9b3ceb80da4750fcce3ec352fa0705c0626af2
shunyue1320 <1277952981@qq.com>
2020-12-04 23:59:43 +0800

    解析标题文件

---
+++ packages/markdown/middleware/title.js
@@ -0,0 +1,5 @@
+module.exports = async ({ fileNode }, next) => {
+  const match = /^#\s?([^#\n\r]*)/.exec(fileNode.body)
+  fileNode.title = match ? match[1].trim() : fileNode.path
+  await next()
+}
