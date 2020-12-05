de8044d513c67e578ef2d270938ef5ac88184d02
shunyue1320 <1277952981@qq.com>
2020-12-05 20:47:41 +0800

    title.js获取标题

--- packages/markdown/middleware/title.js
+++ packages/markdown/middleware/title.js
@@ -1,3 +1,7 @@
+/**
+ * 获取标题
+ */
+
 module.exports = async ({ fileNode }, next) => {
   const match = /^#\s?([^#\n\r]*)/.exec(fileNode.body)
   fileNode.title = match ? match[1].trim() : fileNode.path
