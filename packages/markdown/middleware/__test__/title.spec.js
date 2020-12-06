31eebc5ec271a84c19278c088e3ed3354ce3341c
shunyue1320 <1277952981@qq.com>
2020-12-06 18:33:30 +0800

    头部标题测试用例

---
+++ packages/markdown/middleware/__test__/title.spec.js
@@ -0,0 +1,75 @@
+const {
+  Provider,
+  FileNode,
+  resolvePath,
+  testFile,
+  options,
+  next,
+  updateTestFile
+} = require('../../provider/__test_files__')
+const titleMiddleware = require('../index')
+
+it('单文件 直接调用测试', async () => {
+  const fileNode = new FileNode(testFile, options)
+  await titleMiddleware({
+      fileNode
+  }, next)
+  expect(fileNode.title)
+      .toBe('Test')
+})
+
+it('单文件 无标题测试', async () => {
+  const fileNode = new FileNode('abc/123/test.md', options)
+  await titleMiddleware({
+      fileNode
+  }, next)
+  expect(fileNode.title)
+      .toBe('abc/123/test.md')
+})
+
+it('注册中间件 方式测试', async () => {
+  const provider = new Provider()
+  const testFiles = [testFile]
+  provider.resolvePath = resolvePath
+
+  provider.useMiddleware(titleMiddleware)
+
+  // 初始化
+  await provider.patch(testFiles)
+  expect(provider
+      .fileNodes()
+      .map(item => item.title)
+      .join(', ')
+  ).toBe("Test")
+
+  // 增加文件
+  testFiles.push('abc/123/README.md') // 有标题文件
+  testFiles.push('abc/123/test.md') // 无标题文件
+  await provider.patch(testFiles)
+  expect(provider
+      .fileNodes()
+      .map(item => item.title)
+      .join(', ')
+  ).toBe("ABC_123_README, abc/123/test.md, Test")
+
+  // 删除文件
+  testFiles.pop()
+  await provider.patch(testFiles)
+  expect(provider
+      .fileNodes()
+      .map(item => item.title)
+      .join(', ')
+  ).toBe("ABC_123_README, Test")
+
+  // 文件修改
+  await updateTestFile('# Hello World\nFoo Bar!')
+  await provider.patch(testFiles)
+  expect(provider
+      .fileNodes()
+      .map(item => item.title)
+      .join(', ')
+  ).toBe("ABC_123_README, Hello World")
+
+  // 文件复原
+  await updateTestFile()
+})
